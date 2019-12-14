99a7907b33fd05de117c4bc1a960a57842169ed3
YashviGosar <58715388+YashviGosar@users.noreply.github.com>
2019-12-14 09:16:04 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/*First Slider*/
+$('.slider-one').slick({
+    dots:true,
+    arrows:false,
+    autoplay:true,
+    autoplaySpeed:2000
+}); 
+
+
+$('.slider-two').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots:true,
+    autoplay:true,
+    arrows:false
+  });
